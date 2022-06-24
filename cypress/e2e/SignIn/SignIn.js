4ecb472504b9c2f4f11ce72c1ab79e7ea1b42b53
Jutt247Labs <mail2ishaq101@gmail.com>
2022-06-24 22:25:59 +0500

    mani

--- cypress/e2e/SignIn/SignIn.js
+++ cypress/e2e/SignIn/SignIn.js
@@ -6,7 +6,8 @@ describe('Verify the Sign In page',function(){
      it('SignIn With New User',function(){
         cy.visit('https://app-traxidy.thedemo.co/login/');
         signInPage.signInWithNewUser();
-        //mani dobara pull kr
+//mani dobara pull kr
+// mani change kr yr
         //Hi I am AR Mani
         // Hi I am Ishaq.
     })
